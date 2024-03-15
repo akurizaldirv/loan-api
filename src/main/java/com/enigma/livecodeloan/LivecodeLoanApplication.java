97900f8076ba2bd315692a34c20b7d2fc57f8ef5
zaldi <akurizaldirv@gmail.com>
2024-03-15 09:39:05 +0700

    [INITIALIZE] initialize project

---
+++ src/main/java/com/enigma/livecodeloan/LivecodeLoanApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.livecodeloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivecodeLoanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivecodeLoanApplication.class, args);
+    }
+
+}
